02fca0a776f4b9b241358240fb08ed2cb84142be
Carlos de Moura <lcbarrosdemoura@gmail.com>
2025-07-31 16:21:42 -0300

    + doc of last commit

--- R/cmd.R
+++ R/cmd.R
@@ -5,10 +5,11 @@
 #' @returns list
 #' 
 #' @export
-extract_from_csv = function(fit) {
+extract_cmd = function(fit) {
   fac  = fit$metadata$stan_variable_sizes$alpha[2]
   row  = fit$metadata$stan_variable_sizes$alpha[1]
   col  = fit$metadata$stan_variable_sizes$lambda[2]
+  pred = fit$metadata$stan_variable_sizes$pred[1]
   iter = fit$metadata$iter_sampling
   
   samp = list(
@@ -16,6 +17,13 @@ extract_from_csv = function(fit) {
     lambda = array(dim=c(iter,fac,col)),
     sigma2 = array(dim=c(iter,row,1))
   )
+  
+  if (pred > 1) {  ## INCORRETO
+    samp$pred = array(dim=c(iter,pred,1))
+    for (i in 1:pred) {
+      samp$pred[,i,1] = fit$post_warmup_draws[,,paste0("pred[", i , ",1]")] |> c()
+    }
+  }
   
   for (k in 1:fac) {
     for (i in 1:row) {
